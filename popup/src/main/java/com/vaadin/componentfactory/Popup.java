7441c898c2d0bff128db588d72b846e0a22eba8e
Paola De Bartolo <paola.debartolo@gmail.com>
2024-02-14 17:05:36 -0300

    fix: fix header and footer getting lost when using preserve on refresh

--- popup/src/main/java/com/vaadin/componentfactory/Popup.java
+++ popup/src/main/java/com/vaadin/componentfactory/Popup.java
@@ -148,7 +148,13 @@ public class Popup extends Component implements HasThemeVariant<PopupVariant> {
             if (id == null && this.target == null) {
                 getElement().callJsFunction("disconnectedCallback");
             } else {
-                getElement().callJsFunction("connectedCallback");              
+                getElement().callJsFunction("connectedCallback");
+                if(this.getHeader().isRendererCreated()) {
+                  this.getHeader().initRenderer();
+                }
+                if(this.getFooter().isRendererCreated()) {
+                  this.getFooter().initRenderer();
+                }               
             }
         }
     }
